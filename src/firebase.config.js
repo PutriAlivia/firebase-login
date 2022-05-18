4c7bd1dd3392968c67bd9a32fb15e8c9af7a8d8e
Putri Alivia <putrialivia666@gmail.com>
2022-05-18 19:24:33 +0700

    Firebase login js10

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAuMnQr6HJImYzZSU2LcHO0bGYwC6uxzzc",
+    authDomain: "fir-login-f12e6.firebaseapp.com",
+    projectId: "fir-login-f12e6",
+    storageBucket: "fir-login-f12e6.appspot.com",
+    messagingSenderId: "404831594008",
+    appId: "1:404831594008:web:812a6c6063aeae63a2456d",
+    measurementId: "G-VCWWVXSFXT"
+  };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import routes from "./Routes.js";
+import Header from "./Header";
+import "./style.css";
+import firebase from "firebase";
+import firebaseConfig from "./firebase.config.js";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+firebase.initializeApp(firebaseConfig);
+
+export const AuthContext = React.createContext(null);
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState();
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
+    The page? {JSON.stringify (isLoggedIn)}
+      <div className="App">
+        <Router>
+          <Header />
+          <Switch>
+            {routes.map(route => (
+              <Route
+                key ={route.path}
+                  path ={route.path}
+                    exact = {route.exact}
+                      component = {route.main}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  );
+}
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
